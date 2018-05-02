ea16ee74c635ace5a85276611edc5fe592a46c57
douglas ogisi <douglas.ogisi@gmail.com>
2018-05-02 08:44:22 +0100

    concept of a higher order storeProvider

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,20 @@
 const path = require('path');
 
 const config = {
-  entry: ['babel-polyfill', './lib/components/index.js'],
+  resolve: {
+    modules: [
+      path.resolve('./lib'),
+      path.resolve('./node_modules'),
+    ]
+  },
+  entry: ['babel-polyfill', './lib/renderers/dom.js'],
   output: {
     path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js'
   },
   module: {
     rules: [
+      { test: /\.json$/, loader: 'json-loader'},
       { test: /\.(js|jsx)$/, exclude: /node_modules/, use: 'babel-loader' }
     ]
   }
